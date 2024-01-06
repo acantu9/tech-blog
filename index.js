9cd086070829d74bc08bf33e5d7d5265fb2b9129
acantu9 <adrianaacantu@gmail.com>
2024-01-06 15:50:30 -0600

    added Post and BlogPost dependancies and created routes for rendering new post form, creating a new blog post, updating blog post, & deleting blog post

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const server = require('./server');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sequelize = require('./config/connection');
 const Post = require('../models/Post');
+const BlogPost = require('../models/BlogPost');
+const validator = require('./validator');
 
 // Routes
 // Rendering Main Page
@@ -42,6 +44,82 @@ router.get('/post/:id', async (req, res) => {
             post,
             loggedIn: req.session.loggedIn
         });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Rendering the New Post Form
+router.get('/new', (req, res) => {
+    // Pass any necessary data into new post handlebars template.
+    const defaultPostData = {
+       title: "",
+       content: "",
+       // ... other default data values ...
+    };
+   
+    res.render('new-post', {
+       defaultPostData,
+       loggedIn: req.session.loggedIn
+    });
+});
+
+// Creating New Blog Post
+router.post('/new', async (req, res) => {
+    // Create a new blog post in the database with the provided data from the new post form
+   
+    try {
+       const { title, content, author } = req.body;
+   
+       // Call the validation and sanitization functions
+       const { sanitizedTitle, sanitizedContent, sanitizedAuthor } = validator.validateAndSanitize(title, content, author);
+   
+       const newBlogPost = new BlogPost({
+         title: sanitizedTitle,
+         content: sanitizedContent,
+         author: sanitizedAuthor,
+         createdAt: new Date()
+       });
+   
+       await newBlogPost.save();
+       res.redirect('/');
+    } catch (err) {
+       res.status(500).json(err);
+    }
+});
+
+// Updating a Blog Post
+router.put('/update/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { title, content, author } = req.body;
+
+        // Call the validation and sanitization functions
+        const { sanitizedTitle, sanitizedContent, sanitizedAuthor } = validator.validateAndSanitize(title, content, author);
+
+        // Update the specified blog post in the database with the provided data from the edit post form
+        const updatedBlogPost = await BlogPost.findByIdAndUpdate(id, {
+            title: sanitizedTitle,
+            content: sanitizedContent,
+            author: sanitizedAuthor,
+            updatedAt: new Date()
+        }, { new: true });
+
+        res.status(200).json(updatedBlogPost);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Deleting a Blog Post
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        // Delete the specified blog post from the database
+        await BlogPost.findByIdAndDelete(id);
+
+        res.status(200).json({ message: 'Blog post deleted successfully' });
     } catch (err) {
         res.status(500).json(err);
     }
